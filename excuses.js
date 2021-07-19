cc3506f6b6b7e25f816eb0abe15226f49f753c24
fernandoavilesrosado <62623912+fernandoavilesrosado@users.noreply.github.com>
2021-07-19 17:46:16 +0200

    "borro código basura"

--- excuses.js
+++ excuses.js
@@ -1,18 +1,3 @@
-/*function Excuse() {
-
-    /*var who = ['The dog', 'My grandma', 'His turtle', 'My bird'];
-    var action = ['ate', 'peed', 'crushed', 'broke'];
-    var what = ['my homework', 'the keys', 'the car'];
-    let when = ['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying'];
-
-    var who = who[Math.round(Math.random() * (who.length - 1))];
-    var action = action[Math.round(Math.random() * (action.length - 1))];
-    var what = what[Math.round(Math.random() * (what.length - 1))];
-    var when = when[Math.round(Math.random() * (when.length - 1))];
-
-    document.getElementById('excuses').innerHTML = '<div>My ' + who + ' ' + did + '<br> my ' + what + '.</div>'
-}*/
-
 function Excuse() {
 	
 	var quien = ['The dog', 'My grandma', 'His turtle', 'My bird'];
